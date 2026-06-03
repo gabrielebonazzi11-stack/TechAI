51cfd4284d639e0edc30f1a0ca072b502e843e30
gabrielebonazzi11-stack <gabriele.bonazzi11@gmail.com>
2026-06-03 14:15:54 +0200

    Import drawing issue markers enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./enhancers/pdfDrawingUploadEnhancer";
+import { initDrawingIssueMarkersEnhancer } from "./enhancers/drawingIssueMarkersEnhancer";
+
+initDrawingIssueMarkersEnhancer();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
