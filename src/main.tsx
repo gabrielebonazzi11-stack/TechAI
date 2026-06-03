ab57c990ae7c95098142e9a89119e1ecd7d0cc95
gabrielebonazzi11-stack <gabriele.bonazzi11@gmail.com>
2026-06-03 14:46:27 +0200

    Disable drawing issue marker enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./enhancers/pdfDrawingUploadEnhancer";
-import { initDrawingIssueMarkersEnhancer } from "./enhancers/drawingIssueMarkersEnhancer";
-
-initDrawingIssueMarkersEnhancer();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
